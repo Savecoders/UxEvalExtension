1c683abe03e338919ec648a8324edda81d396300
Savecoders <pablopincay2003@gmail.com>
2024-12-14 15:51:18 -0500

    setup: default config vite react 19

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/manifest.json',
+          dest: '.',
+        },
+      ],
+    }),
+  ],
+
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: '/index.html',
+      },
+    },
+  },
+});
