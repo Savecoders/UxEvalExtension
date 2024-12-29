52736bf20b4193434ec18a690796aab03432574d
Savecoders <pablopincay2003@gmail.com>
2024-12-29 18:07:48 -0500

    chore: chrome and firefox adapter

---
+++ src/services/chrome.service.ts
@@ -0,0 +1,42 @@
+import AdapterBrowser from '../types/browser.adapter';
+
+class ChromeService implements AdapterBrowser<chrome.tabs.Tab> {
+  private currentTab?: chrome.tabs.Tab;
+
+  constructor() {}
+
+  private async getActiveTab(): Promise<chrome.tabs.Tab> {
+    const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+    if (tabs.length) {
+      return tabs[0];
+    } else {
+      throw new Error('No active tab found');
+    }
+  }
+
+  async getBrowserTab(): Promise<chrome.tabs.Tab> {
+    if (!this.currentTab) {
+      this.currentTab = await this.getActiveTab();
+    }
+    return this.currentTab;
+  }
+
+  async executeScript<R>(callback: () => Promise<R>): Promise<R> {
+    const tab = await this.getBrowserTab();
+
+    return chrome.scripting
+      .executeScript({
+        target: { tabId: tab.id! },
+        func: callback
+      })
+      .then((injectionResults) =>
+        injectionResults.length ? (injectionResults[0].result as R) : <R>null
+      )
+      .catch((error) => {
+        console.error(error);
+        return <R>null;
+      });
+  }
+}
+
+export default ChromeService;
---
+++ src/services/firefox.service.ts
@@ -0,0 +1,41 @@
+import AdapterBrowser from '../types/browser.adapter';
+
+class FirefoxService implements AdapterBrowser<browser.tabs.Tab> {
+  private currentTab?: browser.tabs.Tab;
+
+  constructor() {}
+
+  private async getActiveTab(): Promise<browser.tabs.Tab> {
+    const tabs = await browser.tabs.query({ active: true, currentWindow: true });
+    if (tabs.length > 0) {
+      return tabs[0];
+    } else {
+      throw new Error('No active tab found');
+    }
+  }
+
+  async getBrowserTab(): Promise<browser.tabs.Tab> {
+    if (!this.currentTab) {
+      this.currentTab = await this.getActiveTab();
+    }
+    return this.currentTab;
+  }
+
+  async executeScript<R>(callback: () => void): Promise<R> {
+    const tab = await this.getBrowserTab();
+    return browser.scripting
+      .executeScript({
+        target: { tabId: tab.id! },
+        func: callback
+      })
+      .then((injectionResults) =>
+        injectionResults.length ? (injectionResults[0].result as R) : <R>null
+      )
+      .catch((error) => {
+        console.error(error);
+        return <R>null;
+      });
+  }
+}
+
+export default FirefoxService;
